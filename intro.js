bb903e4d5e5e91129810d15e9da4ecd44f567f39
Muhammed Sazid Ul Bari <132262629+SazidWebDev@users.noreply.github.com>
2023-11-23 18:26:15 +0600

    Delete intro.js

--- intro.js
+++
@@ -1,6 +0,0 @@
-const introContainer = document.getElementById("introCon")
-const createIntroTemplate = () => 
-`<div class="text-center overflow-hidden mt-6 md:mt-24">
-<h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl mt-16">The Hadith Stack</h1>
-<p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 text-center px-6 mb-2">A Hadith collection implemented by Sazid. Find hadiths from different books in one place.</p>`
-introContainer.innerHTML = createIntroTemplate();
